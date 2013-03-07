bc9385bbdfb6264fafd77dbddb76c861c5b06c75
Ryan Leonard <ryan@cs.uoregon.edu>
2013-03-06 22:47:56 -0800

    Finished up actionList.h/c, still need to test them.

---
+++ test_util.c
@@ -0,0 +1,11 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    char *fileName = "testutil.txt";
+
+    int i, ret;
+
+    printf("===\nRunning Test Set for util.c.\n===\n\n");
+}
